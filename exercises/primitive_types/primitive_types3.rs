b3dce8dd135a13a6ebf1c61e2b87c69dcc756403
saurabh <meetsaurabhdivine@gmail.com>
2023-06-09 08:03:58 +0530

    solved 20 questions

--- exercises/primitive_types/primitive_types3.rs
+++ exercises/primitive_types/primitive_types3.rs
@@ -2,10 +2,13 @@
 // Create an array with at least 100 elements in it where the ??? is.
 // Execute `rustlings hint primitive_types3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+
 
 fn main() {
-    let a = ???
+    let mut a: [u32; 100]  = [0; 100];
+     for i in 0..100{
+    a[i] = (i + 1) as u32;
+}
 
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
